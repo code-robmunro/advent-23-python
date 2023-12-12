31d260c3beb99f34dd21262366f713e6bbd2ecc5
Rob Munro <code.robmunro@gmail.com>
2023-12-12 05:47:11 +0000

    Add Dockerfile, pandas, pylint, start Day 1 part 2

--- Advent_01/Advent_01.py
+++ Advent_01/Advent_01.py
@@ -42,9 +42,12 @@ Consider your entire calibration document. What is the sum of all of the
 calibration values?
 
 --- Part Two ---
-Your calculation isn't quite right. It looks like some of the digits are actually spelled out with letters: one, two, three, four, five, six, seven, eight, and nine also count as valid "digits".
+Your calculation isn't quite right. It looks like some of the digits are
+actually spelled out with letters: one, two, three, four, five, six, seven,
+eight, and nine also count as valid "digits".
 
-Equipped with this new information, you now need to find the real first and last digit on each line. For example:
+Equipped with this new information, you now need to find the real first and last
+digit on each line. For example:
 
 two1nine
 eightwothree
@@ -53,14 +56,18 @@ xtwone3four
 4nineeightseven2
 zoneight234
 7pqrstsixteen
-In this example, the calibration values are 29, 83, 13, 24, 42, 14, and 76. Adding these together produces 281.
+
+In this example, the calibration values are 29, 83, 13, 24, 42, 14, and 76.
+Adding these together produces 281.
 
 What is the sum of all of the calibration values?
 
 """
 
 import re
-import pandas as pd 
+
+def convert_numberwords_to_digits(input_str: str) -> str:
+    pass
 
 def calculate_calibration(input):
     input = re.sub('[A-Za-z]', '', input)
@@ -77,7 +84,9 @@ def calculate_calibration(input):
 
 
 if __name__ == "__main__":
+    # Test input
     # input = "1abc2\n" "pqr3stu8vwx\n" "a1b2c3d4e5f\n" "treb7uchet\n"
+
     input = open('Advent_01/input.csv').read()
 
     print(calculate_calibration(input))
